7aa5a1c5c59803896e20ff5de73641a399fda317
Poonji <poonjij21@gmail.com>
2023-10-31 22:26:41 +0530

    donw for the day

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
 
@@ -18,13 +14,6 @@ function App() {
   }
   return (
     <>
-    <h1>React</h1>
-    <h2>Counter Value: {counter}</h2>
-    <button onClick={addValue}>
-      Add Value</button>
-    <br/>
-    <button
-onClick={removeValue} >remove value</button>
     </>
   )
 }
