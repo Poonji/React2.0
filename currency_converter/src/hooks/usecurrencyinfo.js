bb0b27f8e4ae847dd845d19e62ec9401b73916b9
Poonji <poonjij21@gmail.com>
2023-11-01 13:24:54 +0530

    making labels

--- currency_converter/src/hooks/usecurrencyinfo.js
+++ currency_converter/src/hooks/usecurrencyinfo.js
@@ -10,6 +10,9 @@ currency-api@1/latest/currencies/${currency}.json`)
 .then((res)=> res.json())
 .then((res)=>setData(res[currency]))
 console.log(data);
-
-  },[])  
+  },[currency]) 
+  console.log(data);
+  return data 
 }
+
+export default useCurrencyInfo;
