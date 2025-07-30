ee89a61a442d601f7f5e40bf0c0b6841d453727b
Poonji <poonjij21@gmail.com>
2025-07-30 17:17:59 +0530

    use effect

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+ import { useState, useCallback } from 'react'
 
 function App() {
   const [length, setLength] = useState(8)
@@ -8,7 +8,7 @@ function App() {
 
   const passwordGenerator = useCallback(()=>{
     let pass=""
-    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let  str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
     if(numberAllowed) str+="0123456789"
     if(charAllowed) str+="!@#$%^&*-_+=[]{}~`'"
@@ -43,15 +43,22 @@ function App() {
             max={100}
             value={length}
             className='cursor-pointer'
+            onChange={(e)=>{setLength(e.target.value)}}
             />
             <label>Length:{length}</label>
-
-
-
-
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={()=>
+            {
+              setNumberAllowed((prev)=>!prev);
+            }
+            }
+            />
           </div>
         </div>
-
       </div>
     </>
   )
